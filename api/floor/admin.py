6e4771a84c48573a0925728c9a4eced5129054de
dfamilia33 <dfamilia33@gmail.com>
2020-03-24 16:31:47 -0400

    Registered floor models into admin view

--- api/floor/admin.py
+++ api/floor/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Floor, Room
 
-# Register your models here.
+class FloorAdmin(admin.ModelAdmin):
+	'''
+	Model for displaying info on admin page for Floor model
+	'''
+	list_display = ('name', 'roomCount')
+
+class RoomAdmin(admin.ModelAdmin):
+	'''
+	Model for displaying info on admin page for Room model
+	'''
+	list_display = ('roomID', 'occupied', 'lastExited', 'lastEntered', 'roomType', 'floor')
+	
+admin.site.register(Floor, FloorAdmin)
+admin.site.register(Room, RoomAdmin)
