6e316acc448631ae718c3a357c227a4d34554e3f
Spencer Planco <spencerplanco@gmail.com>
2020-04-24 18:26:53 -0400

    Changing names of views and correcting url paths

--- api/stats/views.py
+++ api/stats/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_frameowrk import viewsets
+from rest_framework import viewsets
 from .models import Day, Month, Year
 from .serializers import DaySerializer, MonthSerializer, YearSerializer
 
@@ -17,7 +17,7 @@ TODO: fix monthsInfo and yearsInfo and test daysInfo to see if it works, which I
     * month: The month for which the days info is returned (Jan, Feb, ..., Dec)
 @returns - A list of occupancy stats for a given room on each day of a given month in the form [date, totalOccupants, avgOccLength]
 """
-def getDaysInfo(floor, ID, month, viewsets.ModelViewSet):
+class daysView(viewsets.ModelViewSet):
     queryset = Day.objects.all()
     serializer_class = DaySerializer
 
@@ -28,7 +28,7 @@ def getDaysInfo(floor, ID, month, viewsets.ModelViewSet):
     * year: The year for which the months info is returned (2000, 2016, 2020, etc.)
 @returns - A list of occupancy stats for a given room during each month of a given year in the form [date, totalOccupants, avgOccLength]
 """
-def getMonthsInfo(floor, ID, year, viewsets.ModelViewSet):
+class monthsView(viewsets.ModelViewSet):
     queryset = Month.objects.all()
     serializer_class = MonthSerializer
 
@@ -41,6 +41,6 @@ def getMonthsInfo(floor, ID, year, viewsets.ModelViewSet):
 NOTE: Maybe it would be better if we made it so that the user decides a range of years to get that stats for because 
 making it so that it returns ALL the data for EVERY year would be a little much
 """
-def getYearsInfo(floor, ID):
-    queryset = Year.objetcs.all()
+class yearsView(viewsets.ModelViewSet):
+    queryset = Year.objects.all()
     serializer_class = YearSerializer
